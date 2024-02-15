0834b2b3263998b1570d61783cf3a12695a34895
iamlucrative <123393141+iamlucrative@users.noreply.github.com>
2024-02-16 00:36:48 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,14 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void)
+int main()
 {
-float a = 0;
-while (a <= 100)
-{
-    printf("%6.2f degrees F = 6.2f degrees C/n" ,
-           a, (a -32.0) * 5.0 / 9.0 );
-           a = a + 10 ;
-}
+    printf("Hello world!\n");
     return 0;
 }
